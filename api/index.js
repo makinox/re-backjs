6a1278340b8150a0cfa476898e7f0f6c61c45764
Jesús Bossa <x@jesusbossa.dev>
2020-06-17 14:15:05 -0500

    [ADD] Error middleware

--- api/index.js
+++ api/index.js
@@ -3,9 +3,13 @@ const app = express();
 
 const { config } = require('./config/index');
 const mooviesApi = require('./routes/moovies');
+const { errorHandler, logErrors } = require('./utils/middlewares/errorHandlers');
 
 app.use(express.json());
 
 mooviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => console.log(`Ready on: http://localhost:${config.port}`));
