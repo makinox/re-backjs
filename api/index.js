e77df71215ad8aa85147d1a9fb7955aac9577a11
Jesús Bossa <x@jesusbossa.dev>
2020-06-17 21:28:45 -0500

    [ADD] Middlewares

--- api/index.js
+++ api/index.js
@@ -3,13 +3,17 @@ const app = express();
 
 const { config } = require('./config/index');
 const mooviesApi = require('./routes/moovies');
-const { errorHandler, logErrors } = require('./utils/middlewares/errorHandlers');
+const { errorHandler, logErrors, wrapErrors } = require('./utils/middlewares/errorHandlers');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 app.use(express.json());
 
 mooviesApi(app);
 
+app.use(notFoundHandler);
+
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => console.log(`Ready on: http://localhost:${config.port}`));
--- api/utils/middlewares/errorHandlers.js
+++ api/utils/middlewares/errorHandlers.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
+const boom = require('@hapi/boom');
 const { config } = require('../../config/index');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    return { ...error, stack };
   }
 
   return error;
@@ -13,12 +14,26 @@ function logErrors(err, req, res, next) {
   console.log(err);
   next(err);
 }
+
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation());
+  }
+
+  next(err);
+}
+
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload },
+  } = err;
+
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler,
 };
