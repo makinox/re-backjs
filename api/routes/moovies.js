9497bd10367ef777c615542a353f38d372fa0f73
Jesús Bossa <x@jesusbossa.dev>
2020-06-18 12:51:08 -0500

    [ADD] Deploy vercel

--- api/routes/moovies.js
+++ api/routes/moovies.js
@@ -2,6 +2,8 @@ const express = require('express');
 const MoviesService = require('../services/movies');
 const { createMovieSchema, movieIdSchema, updateMovieSchema } = require('../utils/schemas/moovies');
 const validationHandler = require('../utils/middlewares/validationHandler');
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
 
 function moviesApi(app) {
   const router = express.Router();
@@ -10,6 +12,7 @@ function moviesApi(app) {
   const moviesService = new MoviesService();
 
   router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     const { tags } = req.query;
 
     try {
@@ -26,6 +29,7 @@ function moviesApi(app) {
   });
 
   router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
     const { movieId } = req.params;
 
     try {
